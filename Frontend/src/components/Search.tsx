898526a1ae684b8aa74d90bf3fcb6111e711110f
seungJoo <sj10230506@gmail.com>
2024-05-16 16:29:16 +0900

    fix searchbar error

--- Frontend/src/components/Search.tsx
+++ Frontend/src/components/Search.tsx
@@ -2,18 +2,17 @@ import { IoSearch } from "react-icons/io5";
 
 export default function Search(){
 
-    //border-1 rounded 
-    let searchbar = "w-full grid grid-cols-[30px_70px_auto_50px] items-center gap-[5px]"
+    let searchbar = "w-full grid grid-cols-[25px_65px_auto_40px] items-center gap-[5px]"
 
     return(
         <div className={searchbar} >
-            <IoSearch size={"30"}/>
-            <select className="focus:outline-none w-[70px] text-center">
+            <IoSearch size={"25"}/>
+            <select className="focus:outline-none text-center">
                 <option>가게명</option>
                 <option>태그</option>
             </select>
-            <input className="focus:outline-none" placeholder="검색하기.."/>
-            <button>찾기</button>
+            <input className="focus:outline-none w-[100px]" placeholder="검색하기.."/>
+            <button className="w-10">찾기</button>
         </div>
     )
 }
