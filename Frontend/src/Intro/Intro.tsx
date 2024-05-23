cd0df37abe456c7972b6a7a16f9ed9502c6e4b36
seungJoo <sj10230506@gmail.com>
2024-05-24 03:31:59 +0900

    fix url

--- Frontend/src/Intro/Intro.tsx
+++ Frontend/src/Intro/Intro.tsx
@@ -5,7 +5,7 @@ export default function Intro(){
 
     return(
         <div>
-            <a className="mt-[300px]" href="https://mukjaview.kro.kr/oauth2/authorization/google?redirect_uri=http://localhost:3000/signup">
+            <a className="mt-[300px]" href="https://mukjaview.kro.kr/oauth2/authorization/google?redirect_uri=https://mukjaview.kro.kr/signup">
                 구글로 시작하기
             </a>
         </div>
