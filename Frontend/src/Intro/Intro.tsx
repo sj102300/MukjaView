aa23c9bb5f50df046ad22eea31f4e255ddb046ee
seungJoo <sj10230506@gmail.com>
2024-06-07 16:48:39 +0900

    fix url

--- Frontend/src/Intro/Intro.tsx
+++ Frontend/src/Intro/Intro.tsx
@@ -7,7 +7,7 @@ export default function Intro() {
             <p className={styles.title}>당신만을 위한 <br />맞춤 레스토랑 리뷰 서비스</p>
             <img src={"/icons/logo-transparent.png"} width="250px" height="250px" alt="먹자VIEW" />
             {/* <a className={styles.googleBtn} href="https://mukjaview.kro.kr/oauth2/authorization/google?redirect_uri=http://localhost:3000/signup"> */}
-                <a className={styles.gooleBtn} href="https://mukjaview.kro.kr/oauth2/authorization/google?redirect_uri=https://mukjaview.kro.kr/signup">
+                <a className={styles.googleBtn} href="https://mukjaview.kro.kr/oauth2/authorization/google?redirect_uri=https://mukjaview.kro.kr/signup">
                 <div className={styles.icon}>
                     <img width={"30px"} height={"30px"} src="/g-logo.png" alt="Google icon" />
                 </div>
