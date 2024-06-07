32b18ce3075909fc9fb08b2e1d9c8db05491b2dc
seungJoo <sj10230506@gmail.com>
2024-06-07 14:25:59 +0900

    update intro page

--- Frontend/src/Intro/Intro.tsx
+++ Frontend/src/Intro/Intro.tsx
@@ -1,14 +1,19 @@
+import { Link } from "react-router-dom"
+import styles from './intro.module.css'
 
+export default function Intro() {
 
-export default function Intro(){
-
-
-    return(
-        <div>
-            <a className="mt-[300px]" href="https://mukjaview.kro.kr/oauth2/authorization/google?redirect_uri=https://mukjaview.kro.kr/signup">
-            {/* <a className="mt-[300px]" href="https://mukjaview.kro.kr/oauth2/authorization/google?redirect_uri=http://localhost:3000/signup"> */}
+    return (
+        <div className="flex flex-col w-full h-[100dvh] gap-5 justify-center items-center">
+            <p className={styles.title}>당신만을 위한 <br />맞춤 레스토랑 리뷰 서비스</p>
+            <img src={"/icons/logo-transparent.png"} width="250px" height="250px" alt="먹자VIEW" />
+            <Link className={styles.googleBtn} to="https://mukjaview.kro.kr/oauth2/authorization/google?redirect_uri=http://localhost:3000/signup">
+                {/* <Link to="https://mukjaview.kro.kr/oauth2/authorization/google?redirect_uri=https://mukjaview.kro.kr/signup"> */}
+                <div className={styles.icon}>
+                    <img src="/g-logo.png" alt="Google icon" />
+                </div>
                 구글로 시작하기
-            </a>
+            </Link>
         </div>
     )
 }
