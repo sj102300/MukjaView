53bfc0d7a02af873bf8f9eb75ec679698eb216cf
seungJoo <sj10230506@gmail.com>
2024-06-13 18:38:54 +0900

    add toast in signup

--- Frontend/src/SignUp/SignUp.tsx
+++ Frontend/src/SignUp/SignUp.tsx
@@ -4,13 +4,14 @@ import { FirstPage, SecondPage, ThirdPage, FifthPage, SixthPage, LastPage } from
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination } from 'swiper/modules';
+import 'react-toastify/dist/ReactToastify.css';
 
 import './swiper.css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination'
 import FourthPage from './FourthPage';
 import Check from './check';
-import { ToastContainer } from 'react-toastify';
+import { Bounce, ToastContainer } from 'react-toastify';
 
 export interface UserInputInfo {
   identifier: string;
@@ -38,56 +39,59 @@ export function SignUp() {
 
   let [step, setStep] = useState<number>(0);
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log(selectedFile);
-  },[selectedFile])
+  }, [selectedFile])
 
-  switch (step) {
-    case 0:
-      return <Check setStep={setStep} setIdentifier={setIdentifier} setSmileImageUrl={setSmileImageUrl} setSadImageUrl={setSadImageUrl} setNeutralImageUrl={setNeutralImageUrl}/>
-    case 1:
-      return <>
+  return (
+    <>
       <ToastContainer
-        position="top-left" // 알람 위치 지정
+        position="top-center" // 알람 위치 지정
         autoClose={2000} // 자동 off 시간
         hideProgressBar={false} // 진행시간바 숨김
+        transition={Bounce}
         closeOnClick // 클릭으로 알람 닫기
         rtl={false} // 알림 좌우 반전
         pauseOnFocusLoss // 화면을 벗어나면 알람 정지
         draggable // 드래그 가능
-        pauseOnHover // 마우스를 올리면 알람 정지
-        theme="light"
-        limit={1} // 알람 개수 제한
+        theme="colored"
+        className="m-2 flex justify-center items-center text-center"
+      // limit={1} // 알람 개수 제한
       />
-      <Swiper
-        allowTouchMove={false}
-        pagination={{
-          type: 'progressbar',
-        }}
-        modules={[Pagination, Navigation]}
-      >
-        <SwiperSlide><FirstPage setNickname={setNickname} /></SwiperSlide>
-        <SwiperSlide><SecondPage selectedFile={selectedFile} setPreviewUrl={setPreviewUrl} setSelectedFile={setSelectedFile} /></SwiperSlide>
-        <SwiperSlide><ThirdPage selectedFile={selectedFile} previewUrl={previewUrl} setSelectedFile={setSelectedFile} setSmileImageUrl={setSmileImageUrl} setSadImageUrl={setSadImageUrl} setNeutralImageUrl={setNeutralImageUrl} /></SwiperSlide>
-        <SwiperSlide><FourthPage setMBTIOrder={setMBTIOrder} /></SwiperSlide>
-        <SwiperSlide><FifthPage setIsResonable={setIsResonable} setStep={setStep} /></SwiperSlide>
-      </Swiper>
-      </>
-    case 2:
-      return <SixthPage userInputInfo={{
-        identifier: identifier,
-        nickname: nickname,
-        MBTIOrder: MBTIOrder,
-        isReasonable: isResonable,
-        smileImageUrl: smileImageUrl,
-        sadImageUrl: sadImageUrl,
-        neutralImageUrl: neutralImageUrl,
-      }} setStep={setStep} selectedFile={selectedFile} />
-    case 3:
-      return <LastPage />
-    default:
-      null;
-
-  }
-
+      {(() => {
+        switch (step) {
+          case 0:
+            return <Check setStep={setStep} setIdentifier={setIdentifier} setSmileImageUrl={setSmileImageUrl} setSadImageUrl={setSadImageUrl} setNeutralImageUrl={setNeutralImageUrl} />
+          case 1:
+            return <Swiper
+              allowTouchMove={false}
+              pagination={{
+                type: 'progressbar',
+              }}
+              modules={[Pagination, Navigation]}
+            >
+              <SwiperSlide><FirstPage setNickname={setNickname} /></SwiperSlide>
+              <SwiperSlide><SecondPage selectedFile={selectedFile} setPreviewUrl={setPreviewUrl} setSelectedFile={setSelectedFile} /></SwiperSlide>
+              <SwiperSlide><ThirdPage selectedFile={selectedFile} previewUrl={previewUrl} setSelectedFile={setSelectedFile} setSmileImageUrl={setSmileImageUrl} setSadImageUrl={setSadImageUrl} setNeutralImageUrl={setNeutralImageUrl} /></SwiperSlide>
+              <SwiperSlide><FourthPage setMBTIOrder={setMBTIOrder} /></SwiperSlide>
+              <SwiperSlide><FifthPage setIsResonable={setIsResonable} setStep={setStep} /></SwiperSlide>
+            </Swiper>
+          case 2:
+            return <SixthPage userInputInfo={{
+              identifier: identifier,
+              nickname: nickname,
+              MBTIOrder: MBTIOrder,
+              isReasonable: isResonable,
+              smileImageUrl: smileImageUrl,
+              sadImageUrl: sadImageUrl,
+              neutralImageUrl: neutralImageUrl,
+            }} setStep={setStep} selectedFile={selectedFile} />
+          case 3:
+            return <LastPage />
+          default:
+            null;
+        }
+      })()}
+    </>
+  );
 }
